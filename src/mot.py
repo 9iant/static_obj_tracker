a07fed66364b47037dce8c7ff6c984c3e52da174
BGS <baegs94@unist.ac.kr>
2021-08-18 03:34:10 +0900

    many comment ver

--- src/mot.py
+++ src/mot.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import sys
 
 #----------------for offline process---------
-import numpy 
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
@@ -21,40 +21,66 @@ class mot:
     def __init__(self):
         # self.depth_sub = rospy.Subscriber("/depth_estimator/depth_infoarray", depth_infoArray, self.detection_cb)
         # publisher or srv server 
-        self.distance_threshold = 1.0 # [m]
-        self.mot_tracker = Sort(dist_threshold=self.distance_threshold) ## 
+        self.distance_threshold = 1.5 # [m]
+        self.soldier_tracker = Sort(dist_threshold=self.distance_threshold) ## 
+        self.dog_tracker = Sort(dist_threshold=self.distance_threshold)
         # self.mot_Array_pub = rospy.Publisher("/mot/tracks", MarkerArray, queue_size = 1)
 
-    def detection_cb(self, data):
-        # TODO: 
-        if len(data.results)>0:
-        # only dog now.
-            only_dog_indice = []
-        for idx in range(len(data.results)):
-            if data.results[idx].label == "dog":
-                only_dog_indice.append(idx)
-        if len(only_dog_indice):
-            print("{} dogs are detected! ".format(len(only_dog_indice)))
-            dets = np.zeros((len(only_dog_indice), 2))
-            for i in only_dog_indice:
-                dets[i][0] = data.results[i].x
-                dets[i][1] = data.results[i].y
-            self.mot_tracker.update(dets)##
-        self.publish_markerArray()
+    def detection_cb(self, ls_data_item):
+        # if len(ls_data_item)>0:
+        # # only soldier now.
+        #     only_soldier_indice = []
+        # for idx in range(len(ls_data_item)):
+        #     if ls_data_item[idx].yolo_label == "soldier":
+        #         only_soldier_indice.append(idx)
+        # if len(only_soldier_indice):
+        #     # print("{} soldier is detected! ".format(len(only_soldier_indice)))
+        #     dets = np.zeros((len(only_soldier_indice), 2))
+        #     for i in only_soldier_indice:
+        #         dets[i][0] = ls_data_item[i].object_global_position_2d_x
+        #         dets[i][1] = ls_data_item[i].object_global_position_2d_y
+        #     # print(dets)
+        
+        only_soldier = filter(lambda item: item.yolo_label=='soldier',ls_data_item)
+        only_dog = filter(lambda item: item.yolo_label=='dog',ls_data_item)
+        print("current seq id : ", ls_data_item[0].seq_tw)
+        if len(only_soldier)!=0:
+            print("update soldier")
+            self.soldier_tracker.update(only_soldier)
 
+        if len(only_dog)!=0:
+            print("update dog")
+            self.dog_tracker.update(only_dog)
 
+    def print_result(self):
+        print("-"*10)
+        print("the tracking result <soldier>")
+        print("-"*10)
+        for i in range(len(self.soldier_tracker.trackers)):
+            print('{}th track'.format(i))
+            print(self.soldier_tracker.trackers[i].position)
+            print('number of assigned data :', len(self.soldier_tracker.trackers[i].dets))
+        print("-"*10)
+        print("the tracking result <dog>")
+        print("-"*10)
+        for i in range(len(self.dog_tracker.trackers)):
+            print('{}th track'.format(i+1))
+            print(self.dog_tracker.trackers[i].position)
+            print('number of assigned data :', len(self.dog_tracker.trackers[i].dets))
+        
 class Sort(object):
-  def __init__(self, dist_threshold=0.3):
+  def __init__(self, dist_threshold=0.3, min_hits = 10):
     """
     Sets key parameters for SORT
     """
     # self.max_age = max_age
-    # self.min_hits = min_hits
+    self.window_size = 10
+    self.min_hits = 5
     self.dist_threshold = dist_threshold
     self.trackers = [] ## important
     self.frame_count = 0
 
-  def update(self, dets):
+  def update(self, data):
     """
     Params:
       dets - a numpy array of detections in the format [[x,y,score],[x,y,score],...]
@@ -62,34 +88,53 @@ class Sort(object):
     Returns the a similar array, where the last column is the object ID.
     NOTE: The number of objects returned may differ from the number of detections provided.
     """
-    self.frame_count += 1
+    dets = np.zeros((len(data), 2))
+    for i in range(len(data)):
+        dets[i][0] = data[i].object_global_position_2d_x
+        dets[i][1] = data[i].object_global_position_2d_y
+
+    self.frame_count += 1 # all data has same seq_tw number. thus choose 0 indice
+
     # get predicted locations from existing trackers.
     trks = np.zeros((len(self.trackers), 2))
     for idx, tracker in enumerate(self.trackers):
     	trks[idx] = self.trackers[idx].position # no predition
-    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(np.array(dets), np.array(trks), self.dist_threshold) ###########3
-    
+    # print('dets', dets)
+    # print('trks', trks)
+    matched, unmatched_dets, unmatched_trks = associate_detections_to_trackers(np.array(dets), np.array(trks), self.dist_threshold) ##
+    '''
+        matched : Assignment matrix,  [<trks>, <dets>] <...> is column matrix. = 2d array
+        unmatched_dets : unmatched dets, [dets1, dets2, ... ] = 1d array
+        unmatched_trks : unmatched trks, [trks1, trks2, ... ] = 1d array
+    '''
+    # print('-'*10)
+    # print('matching result : ')
+    # print(matched)
+    # print('unmatched dets : ')
+    # print(unmatched_dets)
+    # print('unmatched tracks : ')
+    # print(unmatched_trks)
+    # print('-'*10)
+
     # update matched trackers with assigned detections
+    # print("<"*10+"start update"+">"*10)
     for m in matched:
-      print("matching : {}".format(m)) #################### m[0] / m[1] exchaged!
-      print(dets[m[0], :])
-      print(self.trackers[m[1]].position)
-      self.trackers[m[1]].update(dets[m[0], :])
+    #   print("matching : {}".format(m)) #################### m[0] / m[1] exchaged!
+    #   print(dets)
+
+    #   print(dets[m[1], :])
+    #   print(self.trackers[m[0]].position)
+      self.trackers[m[0]].update(data[m[1]])#(dets[m[1], :])
     # create and initialise new trackers for unmatched detections
+    
+    # print(data)
     for i in unmatched_dets:
-        trk = Tracker(dets[i,:])
+        trk = Tracker(data[i])
         self.trackers.append(trk)
         print("new Track generated! current total {} tracks".format(Tracker.count))
     i = len(self.trackers)
 
-    # if self.frame_count%150==1: # per 100 frames, delete tracker with few detections 
-    #   del_trk = []
-    #   for idx, tracker in enumerate(self.trackers):
-    #     if tracker.hits < 10:
-    #       del_trk.append(idx)
-    #   for i in del_trk:
-    #   	self.trackers.pop(i) # remove tracker with few detections
-    #   	print("track dead because few detections")
+    
 
     # for trk in reversed(self.trackers):
     #     d = trk.get_state()[0]
@@ -125,14 +170,18 @@ def get_distancemat(dets, trks):
 
 class Tracker(object):
   count = 0
-  def __init__(self, position):
+  def __init__(self, data): #v dets[m[1], :] -> data[m[1]]
     self.id = Tracker.count
-    self.position = np.array(position) # [x, y]
+    self.position = np.array([data.object_global_position_2d_x, data.object_global_position_2d_y]) # [x, y]
     Tracker.count += 1
     self.hits = 1
-  def update(self, position): # just simple moving avg
+    self.dets = list(data)
+  def update(self, data): # just simple moving avg
     self.hits += 1
-    self.position = (self.position*(self.hits-1) + np.array(position))/self.hits
+    new_position = np.array([data.object_global_position_2d_x, data.object_global_position_2d_y])
+    self.position = (self.position*(self.hits-1) + new_position)/self.hits
+    self.dets.append(data)
+
     
 def associate_detections_to_trackers(detections,trackers,distance_threshold = 1.0):
     """
@@ -150,21 +199,24 @@ def associate_detections_to_trackers(detections,trackers,distance_threshold = 1.
     else:
         matched_indices = np.zeros((0,2))
 
+
     unmatched_detections = []
     for d, det in enumerate(detections):
-        if(d not in matched_indices[:,0]):
+        if(d not in matched_indices[:,1]):
             unmatched_detections.append(d)
+            
     unmatched_trackers = []
     for t, trk in enumerate(trackers):
-        if(t not in matched_indices[:,1]):
+        if(t not in matched_indices[:,0]):
             unmatched_trackers.append(t)
     #filter out matched with far distance
-        matches = []
+    
+    matches = []
     
     for m in matched_indices:
         if(distance_matrix[m[0], m[1]] > distance_threshold): # if distance is too far, don't match
-            unmatched_detections.append(m[0])
-            unmatched_trackers.append(m[1])
+            unmatched_detections.append(m[1])
+            unmatched_trackers.append(m[0])
         else:
             matches.append(m.reshape(1,2))
     if(len(matches)==0):
@@ -175,39 +227,39 @@ def associate_detections_to_trackers(detections,trackers,distance_threshold = 1.
     return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
 
 def main(args):
-    
     # read data
-    data = pd.read_csv("./rec/image_server_for_test.csv")
-    print(len(data)) # 1185 for dataset1 
-    data = data.drop(data.index[296:len(data)]) # use [0:295] only for dataset1
+    data = pd.read_csv("./rec/image_server_out1.csv")
+    # cut data
+    data = data.drop(data.index[296:len(data)]) # use [0:295] only for dataset1 (total 1185)
 
-    # stamp = data['stamp'].to_numpy()
-    # stamp = stamp - stamp[0] # Start with 0.0 s
-    # seq_tw = data['seq_tw'].to_numpy()
-    # drone_position_x = data['drone_position_x'].to_numpy()
-    # drone_position_y = data['drone_position_y'].to_numpy()
-    # drone_position_z = data['drone_position_z'].to_numpy()
-    # drone_position_qx = data['drone_position_qx'].to_numpy() 
-    # drone_position_qy = data['drone_position_qy'].to_numpy()
-    # drone_position_qz = data['drone_position_qz'].to_numpy()
-    # drone_position_qw = data['drone_position_qw'].to_numpy()
-    # yolo_label = data['yolo_label'].to_numpy()
-    # yolo_xmin = data['yolo_xmin'].to_numpy()
-    # yolo_xmax = data['yolo_xmax'].to_numpy()
-    # yolo_ymin = data['yolo_ymin'].to_numpy()
-    # yolo_ymax = data['yolo_ymax'].to_numpy()
-    # yolo_confidence = data['yolo_confidence'].to_numpy()
-    # object_global_position_2d_x = data['object_global_position_2d_x'].to_numpy()
-    # object_global_position_2d_y = data['object_global_position_2d_y'].to_numpy()
-    # object_global_position_2d_z = data['object_global_position_2d_z'].to_numpy()
-
+    # change it to list type data container
+    ls_data = []
+    result = []
+    index = 0
+    for key, value in data.iterrows():
+        if (index+2)> len(data): # to deal with last item
+            break
+        result.append(value)
+        if value.seq_tw != data.loc[index+1,'seq_tw']: # if the seq_tw is same, store them into one item.
+            ls_data.append(result)
+            result = []    
+        index += 1
+    
     ## MOT initialization ##
     mot_tracker = mot()
+    ## pseudo topic generation and run callback
+    for ls_data_item in ls_data:
+        mot_tracker.detection_cb(ls_data_item)
 
-    data_list = []
-    for key, value in data.iterrows():
-        data_list.append(value)
-        pass
+
+    # TODO: delete tracks..
+    
+    for idx, tracker in enumerate(mot_tracker.dog_tracker.trackers):
+        if tracker.hits < mot_tracker.dog_tracker.min_hits :
+            mot_tracker.dog_tracker.trackers.remove(tracker)
+            print("track dead because few detections")
+
+    mot_tracker.print_result()
     '''                   Draw (batch)           '''
     # fig = plt.figure()
     # ax = fig.add_subplot(1,1,1)
